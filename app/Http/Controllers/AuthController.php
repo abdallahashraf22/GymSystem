a279d1160274f070f0440639de5bf4e70a226a37
Hassan Mohamed <hassan.mhmd656@gmail.com>
2022-03-24 02:02:44 +0200

    add JWT

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     * func
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except(["login"]);
+    }
+
+    public function login()
+    {
+        $user = User::where("email", "=", request()->email, "and", "password", "=", request()->password)->first();
+
+        $credentials = request(['email', 'password']);
+
+        // JWTAuth::fromUser($user)
+        $customClaims = [
+            'email' => $user->email,
+            'role' => $user->role,
+        ];
+
+        if (!$token = JWTAuth::claims($customClaims)->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => Auth::factory()->getTTL() * 60
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CityManager;
@@ -15,11 +16,22 @@ use App\Http\Controllers\CityManager;
 |
 */
 
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/citybranches',[CityManager::class, 'getAllBranches']);
-Route::post('/createbranch',[CityManager::class, 'createBranch']);
-Route::post('/editbranch/{branchId}',[CityManager::class, 'editBranch']);
-Route::delete('/deletebranch/{branchId}',[CityManager::class, 'deleteBranch']);
+Route::get('/citybranches', [CityManager::class, 'getAllBranches']);
+Route::post('/createbranch', [CityManager::class, 'createBranch']);
+Route::post('/editbranch/{branchId}', [CityManager::class, 'editBranch']);
+Route::delete('/deletebranch/{branchId}', [CityManager::class, 'deleteBranch']);
+
+##### for JWT Auth ######
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', [AuthController::class, "login"]);
+});
+###########################
