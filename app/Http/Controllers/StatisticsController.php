5e1b25ab02de4a10feb1c0e64aa70f4b90556f57
Hassan Mohamed <hassan.mhmd656@gmail.com>
2022-04-17 01:31:49 +0200

    get top 10 users

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Traits\ResponseTrait;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class StatisticsController extends Controller
 {
+    use ResponseTrait;
+
     public function __construct()
     {
         $this->middleware("auth")->except([]);
@@ -41,22 +45,27 @@ class StatisticsController extends Controller
 
     public function getTopUsers(Request $request)
     {
-        $topUsers = DB::table("packages_users_branches as pub")
-            ->select(DB::raw("name"), DB::raw("sum(package_sessions) as 'total_sessions'"))
-            ->groupBy("name")
-            ->orderByDesc("total_sessions")
-            ->join("users as u", "pub.user_id", "=", "u.id")
-            // ->join('cities as c', 'c.id', '=', 'b.city_id')
-            // ->when(request('city_id') != 'all', function ($query) {
-            //     $query->where(function ($q) {
-            //         $q->where("c.id", request("city_id"));
-            //     });
-            // })->when(request('branch_id') != 'all', function ($query) {
-            //     $query->where(function ($q) {
-            //         $q->where("b.id", request("branch_id"));
-            //     });
-            // })
-            ->get();
-        return response()->json($topUsers);
+        try {
+            $topUsers = DB::table("packages_users_branches as pub")
+                ->join("users as u", "pub.user_id", "=", "u.id")
+                ->join('branches as b', 'pub.branch_id', '=', 'b.id')
+                ->join('cities as c', 'c.id', '=', 'b.city_id')
+                ->groupBy("pub.user_id", "u.name")
+                ->when(request('city_id') != 'all', function ($query) {
+                    $query->where(function ($q) {
+                        $q->where("c.id", request("city_id"));
+                    });
+                })->when(request('branch_id') != 'all', function ($query) {
+                    $query->where(function ($q) {
+                        $q->where("b.id", request("branch_id"));
+                    });
+                })
+                ->select("pub.user_id", "u.name", DB::raw("SUM(pub.enrollement_price) as totoal_price"), DB::raw("SUM(pub.remianing_sessions) as totoal_sessions"))
+                ->orderByDesc(DB::raw("SUM(pub.enrollement_price)"))->limit(10)->get();
+        } catch (Exception $e) {
+            return $this->createResponse(200, [], false, "server error");
+        }
+
+        return $this->createResponse(200, $topUsers);
     }
 }
